cb7888e8c621a0a829f7ee93587c499083dea291
yakontik <yanakoptux@gmail.com>
2023-01-29 20:53:31 +0200

    init

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello")
+class me():
+    count_me=0
+    def __init__(self, name, age):
+        self.name=name
+        self.age=age
+    
+
+    def sit(self):
+        print(self.name+ " is study now")
+
+
+myme=me("Yana",14)
+print(f"Yana name: {myme.name}. Age: {myme.age}")
+myme.sit()
+
+myme1=me("Yana",15)
+print(f"Yana name: {myme1.name}. Age: {myme1.age}")
+myme1.sit()
+
+
