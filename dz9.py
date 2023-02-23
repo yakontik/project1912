1bf05f5a730e316215278045c6778737abf7c81c
yakontik <yanakoptux@gmail.com>
2023-02-23 15:48:06 +0200

    init

---
+++ dz9.py
@@ -0,0 +1,13 @@
+def make_shirt(size, message):
+    print(f"The size of the shirt is {size}.")
+    print(f"The message on the shirt is '{message}'.")
+make_shirt("S", "I love Step")
+make_shirt("L", "I love Ukraine")
+make_shirt("M", "I love Rivne")
+
+
+def describe_city(city, country):
+    print(f"{city} is in {country}.")
+describe_city("Rivne", "Ukraine")
+describe_city("Seul", "Korea")
+describe_city("Tokyo", "Japan")
